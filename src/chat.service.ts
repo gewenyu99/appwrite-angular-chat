775b8e1a6259f9ea4ee367ee55fafcfae43f735f
Vincent (Wen Yu) Ge <wenyu1999@hotmail.com>
2023-04-28 22:47:09 +0000

    test in 1.3.x

---
+++ src/chat.service.ts
@@ -0,0 +1,69 @@
+import { inject, Injectable } from '@angular/core';
+import { ID, Models, Permission, Query, RealtimeResponseEvent, Role } from 'appwrite';
+import { BehaviorSubject, take, concatMap, filter } from 'rxjs';
+
+import { AppwriteApi, AppwriteEnvironment } from './appwrite';
+import { AuthService } from './auth.service';
+
+export type Message = Models.Document & {
+  user: string;
+  message: string;
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ChatService {
+  private appwriteAPI = inject(AppwriteApi);
+  private appwriteEnvironment = inject(AppwriteEnvironment);
+
+  private _messages$ = new BehaviorSubject<Message[]>([]);
+  readonly messages$ = this._messages$.asObservable();
+
+  constructor(private authService: AuthService) {}
+
+  loadMessages() {
+    this.appwriteAPI.database
+      .listDocuments<Message>(
+        this.appwriteEnvironment.databaseId,
+        this.appwriteEnvironment.chatCollectionId,
+        [Query.limit(100)]
+      )
+      .then((response) => {
+        this._messages$.next(response.documents);
+      });
+  }
+
+  sendMessage(message: string) {
+    return this.authService.user$.pipe(
+      filter((user) => !!user),
+      take(1),
+      concatMap((user) => {
+        const data = {
+          user: user!.name,
+          message,
+        };
+
+        return this.appwriteAPI.database.createDocument(
+            this.appwriteEnvironment.databaseId,
+            this.appwriteEnvironment.chatCollectionId,
+            ID.unique(),
+            data,
+            [Permission.read(Role.any()), Permission.write(Role.user(user!.$id))]
+        );
+      })
+    );
+  }
+  listenToMessages() {
+    return this.appwriteAPI.database.client.subscribe(
+      `databases.chat.collections.messages.documents`,
+      (res: RealtimeResponseEvent<Message>) => {
+        if (res.events.includes('databases.chat.collections.messages.documents.*.create')) {
+          const messages: Message[] = [...this._messages$.value, res.payload];
+ 
+          this._messages$.next(messages);
+        }
+      }
+    );
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  endpoint: 'http://localhost/v1',
+  projectId: 'ngchat',
+  databaseId: 'chat',
+  chatCollectionId: 'messages',
   production: false
-};
+ };
 
 /*
  * For easier debugging in development mode, you can import the following file
